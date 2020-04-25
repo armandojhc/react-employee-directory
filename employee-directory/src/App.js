18d43c0196891824231f55d08b00bd66b0c81c54
Armando Herrera Canelon <armando.herrera91@gmail.com>
2020-04-24 22:23:06 -0400

    fixed image rendering

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import API from "./utils/API";
 import Table from 'react-bootstrap/Table';
 import EmployeeRow from './components/EmployeeRow';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -58,18 +59,20 @@ class App extends Component {
     
     console.log(this.state.employees.length);
     return (
-      // <Jumbotron fluid>
-      //     <Container>
-      //       <h1>Fluid jumbotron</h1>
-      //       <p>
-      //         This is a modified jumbotron that occupies the entire horizontal space of
-      //         its parent.
-      //       </p>
-      //     </Container>
-      // </Jumbotron>
+      
+      
       
   
       <Container fluid>
+        <Jumbotron fluid>
+          
+            <h1>Fluid jumbotron</h1>
+            <p>
+              This is a modified jumbotron that occupies the entire horizontal space of
+              its parent.
+            </p>
+          
+      </Jumbotron>
         <Row>
           <Col> <h1> Employee Search App</h1></Col>
         </Row>
@@ -111,6 +114,7 @@ class App extends Component {
           </Col>
         </Row>
       </Container>
+    
     );
   }
 }
--- employee-directory/src/components/EmployeeRow/index.js
+++ employee-directory/src/components/EmployeeRow/index.js
@@ -5,7 +5,7 @@ class EmployeeRow extends Component {
     render() {
         return (
             <tr>
-                <td>1</td>
+                <td><img alt="Employee" src={this.props.picture} /></td>
                 <td>{this.props.name}</td>
                 <td>{this.props.phone}</td>
                 <td>@mdo</td>
